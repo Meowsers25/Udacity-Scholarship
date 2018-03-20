8d78442c2f22c7d46c31460bf9627c14dff843ef
Chris <c.lamothe25@gmail.com>
2018-03-19 21:21:19 -0400

    Finished JuliaJames

--- juliaJames.js
+++ juliaJames.js
@@ -4,8 +4,15 @@
 
 var x = 1;
 
-while (/* your stop condition goes here */) {
-    // check divisibility
-    // print Julia, James, or JuliaJames
-    // increment x
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("JuliaJames");
+    } else if (x % 3 === 0){
+        console.log("Julia");
+    } else if (x % 5 === 0) {
+        console.log("James");
+    } else {
+        console.log(x);
+    }
+    x += 1;
 }
