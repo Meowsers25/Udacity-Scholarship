b672f45f5f17e28cfc62c07752d44c93a8d19eae
Chris <c.lamothe25@gmail.com>
2018-04-09 22:08:17 -0400

    Starting Pixel Art Maker

---
+++ pixelMaker/designs.js
@@ -0,0 +1,16 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+let row = document.querySelectorAll('input[value="something"]');
+let col = document.getElementById('inputWeight');
+
+document.getElementById('button').addEventListener('click', function(){
+  console.log(row);
+  // console.log(col);
+});
+function makeGrid() {
+
+// Your code goes here!
+
+}
