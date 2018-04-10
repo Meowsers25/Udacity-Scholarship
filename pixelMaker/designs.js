e7a2547ad018240429bb805d23a8f98279e287ea
Chris <c.lamothe25@gmail.com>
2018-04-09 22:32:51 -0400

    Working on grid size in vanilla JS

--- pixelMaker/designs.js
+++ pixelMaker/designs.js
@@ -2,13 +2,20 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let row = document.querySelectorAll('input[value="something"]');
+let row = document.getElementById('inputHeight');
 let col = document.getElementById('inputWeight');
 
-document.getElementById('button').addEventListener('click', function(){
-  console.log(row);
-  // console.log(col);
+document.querySelector('form#sizePicker').addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log(row.value);
 });
+
+document.querySelector('form#sizePicker').addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log(col.value);
+});
+
+
 function makeGrid() {
 
 // Your code goes here!
